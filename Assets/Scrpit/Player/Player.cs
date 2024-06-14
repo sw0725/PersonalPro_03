dccb75ece8cbe6a778ef09aabadc1ecfb40aeda9
yeolchiyang <yangchi97@naver.com>
2024-06-14 15:19:24 +0900

    PlayerMove_2

--- Assets/Scrpit/Player/Player.cs
+++ Assets/Scrpit/Player/Player.cs
@@ -12,8 +12,6 @@ public class Player : MonoBehaviour
     public float runSpeed = 5.0f;
     public float currentSpeed = 3.0f;
 
-    bool isMove = false;
-
     Vector2 inputDir = Vector2.zero;
 
     //점프=============================================
@@ -25,6 +23,32 @@ public class Player : MonoBehaviour
     float jumpTimer = 0.0f;
     bool jumpKey = false;           //점프 높이조절 위함
     bool isJumping = false;
+
+    //상태=============================================
+
+    enum PlayerState : byte
+    {
+        Idle =0,
+        Move,
+        Wall,
+        Vaine,
+        Shoot,
+    }
+
+    PlayerState State 
+    {
+        get => state;
+        set 
+        {
+            if(state != value) 
+            {
+                StateExit(state);
+                state = value;
+                StateEnter(state);
+            }
+        }
+    }
+    PlayerState state = PlayerState.Idle;
 
     //체력=============================================
 
@@ -56,6 +80,9 @@ public class Player : MonoBehaviour
 
     //기타=============================================
 
+    float groundRayRange = 0.5f;
+    float wallRayRange = 0.4f;
+
     PlayerInputAction actions;
     Rigidbody2D rb;
     Animator animator;
@@ -79,13 +106,28 @@ public class Player : MonoBehaviour
     {
         if (IsAlive)
         {
-            rb.velocity = new Vector2(inputDir.x * moveSpeed, rb.velocity.y);        //MovePosition은 순간이동이라 중력 적용량이 증발함
+            rb.velocity = new Vector2(inputDir.x * currentSpeed, rb.velocity.y);        //MovePosition은 순간이동이라 중력 적용량이 증발함
         }
 
         if (isJumping && jumpKey && jumpTimer < jumpLimit)
         {
             rb.AddForce(smallJumpPower * ((jumpTimer) + 1f) * Vector2.up, ForceMode2D.Impulse);
             jumpTimer += Time.fixedDeltaTime;
+        }
+
+        Debug.DrawRay(rb.position, Vector2.down * groundRayRange, new(0, 0, 1));
+        Debug.DrawRay(rb.position, inputDir * wallRayRange, new(0, 0, 1));
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)  //콜리젼 말고 레이를 사용해보자 바닥 레이가 그라운드일때와 무릎레이가 월일때 적용
+    {
+        if (collision.gameObject.layer == 7)                //상태 적용, 벽타기 상태에서 땅에 내려왔을때 해결하기
+        {
+            if (collision.gameObject.CompareTag("Wall")) 
+            {
+                WallRay();
+            }
+            GroundRay();
         }
     }
 
@@ -95,20 +137,61 @@ public class Player : MonoBehaviour
         onDie?.Invoke();
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)  //콜리젼 말고 레이를 사용해보자 바닥 레이가 그라운드일때와 무릎레이가 월일때 적용
+    void GroundRay() 
     {
-        if(collision.gameObject.layer == 7) 
+        RaycastHit2D hit2D = Physics2D.Raycast(rb.position, Vector2.down, groundRayRange, LayerMask.GetMask("Ground"));
+        if (hit2D.collider != null)
         {
             isJumping = false;
             if (jumpKey)
             {
-                isJumping = true;
-                jumpKey = true;
-                jumpTimer = 0;
-
-                rb.AddForce(jumpPower * Vector2.up, ForceMode2D.Impulse);
+                Jump();
             }
-            //플랫폼의 옆면에 부딫혔을때 작동 안하는 방법 생각하기
+        }
+    }
+    void WallRay() 
+    {
+        RaycastHit2D hit2D = Physics2D.Raycast(rb.position, inputDir, wallRayRange, LayerMask.GetMask("Ground"));
+        if (hit2D.collider != null)
+        {
+            //벽타기로
+            Debug.Log("WallRay");
+        }
+    }
+
+    //상태처리===========================================
+
+    void StateEnter(PlayerState state)
+    {
+        switch (state) 
+        {
+            case PlayerState.Idle: 
+                break;
+            case PlayerState.Move:
+                break;
+            case PlayerState.Wall:
+                break;
+            case PlayerState.Vaine:
+                break;
+            case PlayerState.Shoot:
+                break;
+        }
+    }
+
+    void StateExit(PlayerState state) 
+    {
+        switch (state)
+        {
+            case PlayerState.Idle:
+                break;
+            case PlayerState.Move:
+                break;
+            case PlayerState.Wall:
+                break;
+            case PlayerState.Vaine:
+                break;
+            case PlayerState.Shoot:
+                break;
         }
     }
 
@@ -121,9 +204,9 @@ public class Player : MonoBehaviour
         actions.Player.Move.canceled += OnMove;
         actions.Player.Run.performed += OnRun;
         actions.Player.Run.canceled += OnRun;
-        actions.Player.UpDown.performed += OnUpDown;
         actions.Player.Jump.performed += OnJump;
         actions.Player.Jump.canceled += OnJump;
+        actions.Player.UpDown.performed += OnUpDown;
         actions.Player.Attack.performed += OnAttack;
         actions.Player.Reload.performed += OnReload;
     }
@@ -133,29 +216,29 @@ public class Player : MonoBehaviour
         actions.Player.Move.canceled -= OnMove;
         actions.Player.Run.performed -= OnRun;
         actions.Player.Run.canceled -= OnRun;
-        actions.Player.UpDown.performed -= OnUpDown;
         actions.Player.Jump.performed -= OnJump;
         actions.Player.Jump.canceled -= OnJump;
+        actions.Player.UpDown.performed -= OnUpDown;
         actions.Player.Attack.performed -= OnAttack;
         actions.Player.Reload.performed -= OnReload;
         actions.Player.Disable();
     }
-    private void OnMove(InputAction.CallbackContext context)
+    private void OnMove(InputAction.CallbackContext context)    //Vaine시에 작동안함
     {
         if (context.canceled)
         {
             inputDir = Vector2.zero;
 
-            isMove = false;
+            State = PlayerState.Idle;
         }
         else
         {
             inputDir = context.ReadValue<Vector2>();
 
-            isMove = true;
+            State = PlayerState.Move;
         }
     }
-    private void OnRun(InputAction.CallbackContext context)
+    private void OnRun(InputAction.CallbackContext context)     //Shoot시에 작동 안함
     {
         if (context.canceled) 
         {
@@ -166,33 +249,44 @@ public class Player : MonoBehaviour
             currentSpeed = runSpeed;    
         }
     }
-    private void OnUpDown(InputAction.CallbackContext context)
+    private void OnJump(InputAction.CallbackContext context)    //Vaine, Wall시에 다르게 점프
     {
-    }
-    private void OnJump(InputAction.CallbackContext context)
-    {
-        Debug.Log(isJumping);
         if (context.canceled)
         {
             jumpKey = false;
         }
         else 
         {
+            jumpKey = true;
             if (!isJumping) 
             {
-                isJumping = true;
-                jumpKey = true;
-                jumpTimer = 0;
-
-                rb.AddForce(jumpPower * Vector2.up, ForceMode2D.Impulse);
-                Debug.Log("jump");
+                Jump();
             }
         }
     }
-    private void OnAttack(InputAction.CallbackContext context)
+    void Jump()
+    {
+        isJumping = true;
+        jumpTimer = 0;
+
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(jumpPower * Vector2.up, ForceMode2D.Impulse);
+    }
+    private void OnUpDown(InputAction.CallbackContext context)
+    {
+    }
+    private void OnAttack(InputAction.CallbackContext context)      //무기 들었을때는 달리기 봉인
     {
     }
     private void OnReload(InputAction.CallbackContext context)
     {
     }
+#if UNITY_EDITOR
+
+    private void OnDrawGizmos()
+    {
+        
+    }
+
+#endif
 }
